87cd6c02d4dee43e1af1e55fba3d1094e9de2543
Gpontes143 <gpontes143@gmail.com>
2026-06-06 01:01:18 -0300

    arrumando espaço

--- src/script.js
+++ src/script.js
@@ -2,17 +2,17 @@ const loginForm = document.getElementById("loginForm");
 const message = document.getElementById("message");
 
 loginForm.addEventListener("submit", function (event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const email = document.getElementById("email").value;
-    const senha = document.getElementById("senha").value;
+  const email = document.getElementById("email").value;
+  const senha = document.getElementById("senha").value;
 
-    // Exemplo simples
-    if (email === "admin@email.com" && senha === "123456") {
-        message.textContent = "Login realizado com sucesso!";
-        message.className = "message success";
-    } else {
-        message.textContent = "E-mail ou senha inválidos!";
-        message.className = "message error";
-    }
+  // Exemplo simples
+  if (email === "admin@email.com" && senha === "123456") {
+    message.textContent = "Login realizado com sucesso!";
+    message.className = "message success";
+  } else {
+    message.textContent = "E-mail ou senha inválidos!";
+    message.className = "message error";
+  }
 });
